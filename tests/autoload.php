f2f073ce97c6ff6fcffe701e3ce890dbdad8fc78
Eugene <test82987@gmail.com>
2025-07-16 18:04:39 +1000

    Update PHPUnit configuration to use tests/autoload.php for bootstrapping and add autoload script for test environment setup + refresh database for testing

---
+++ tests/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$env   = env('APP_ENV', 'not set');
+$db    = env('DB_DATABASE', 'not set');
+
+
+echo '(ツ)' . "\n";
+echo '┌-------------------------------------------------------------┐' . "\n";
+echo '| Env                    :' . $env . "\n";
+echo '| Current UTC time       :' . now() . "\n";
+echo '| Current Melbourne time :' . now()->timezone('Australia/Melbourne') . "\n";
+echo '| Database               :' . $db . "\n";
+echo '└-------------------------------------------------------------┘' . "\n";
+
+echo shell_exec('php artisan migrate:fresh --seed');
